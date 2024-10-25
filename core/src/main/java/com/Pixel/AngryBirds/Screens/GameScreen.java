0b1f7a7209daca29e3cb74707675e10097f35b2c
Abhishek <162453293+abhishek23026@users.noreply.github.com>
2024-10-25 10:37:56 +0530

    Added Load Screen and various other elements

---
+++ core/src/main/java/com/Pixel/AngryBirds/Screens/GameScreen.java
@@ -0,0 +1,82 @@
+package com.Pixel.AngryBirds.Screens;
+
+import com.Pixel.AngryBirds.AngryBirdsGame;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.utils.ScreenUtils;
+import com.badlogic.gdx.utils.viewport.FitViewport;
+import com.badlogic.gdx.utils.viewport.Viewport;
+
+public class GameScreen implements Screen {
+
+    private AngryBirdsGame game;
+    private OrthographicCamera camera;
+    private Viewport viewport;
+    private Texture backgroundTexture;
+    private float timeElapsed = 0;
+
+    private static final float WORLD_WIDTH = 800;
+    private static final float WORLD_HEIGHT = 600;
+
+    public GameScreen(AngryBirdsGame game){
+        this.game = game;
+
+        backgroundTexture = new Texture("loadingScreenBackground.png");
+
+//        camera = new OrthographicCamera();
+//        viewport = new FitViewport(WORLD_WIDTH, WORLD_HEIGHT, camera);
+//        camera.position.set(WORLD_WIDTH / 2, WORLD_HEIGHT / 2, 0);
+        this.camera = game.camera;
+        this.viewport =  game.viewport;
+    }
+
+    @Override
+    public void show() {
+        // Prepare your screen here.
+    }
+
+    @Override
+    public void render(float delta) {
+        Gdx.gl.glClearColor(0, 0, 0, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+        camera.update();
+
+        game.batch.setProjectionMatrix(camera.combined);
+        game.batch.begin();
+        game.batch.draw(backgroundTexture, 0, 0, WORLD_WIDTH, WORLD_HEIGHT);
+        game.batch.end();
+
+    }
+
+    @Override
+    public void resize(int width, int height) {
+        // Resize your screen here. The parameters represent the new window size.
+        viewport.update(width, height);
+    }
+
+    @Override
+    public void pause() {
+        // Invoked when your application is paused.
+    }
+
+    @Override
+    public void resume() {
+        // Invoked when your application is resumed after pause.
+    }
+
+    @Override
+    public void hide() {
+        // This method is called when another screen replaces this one.
+    }
+
+    @Override
+    public void dispose() {
+        // Destroy screen's assets here
+        backgroundTexture.dispose();
+    }
+}
